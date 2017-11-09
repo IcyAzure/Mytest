56a53a77cd46cad3722c0602f9ef82023a19dc35
luyuheng <272472676@qq.com>
2017-11-09 16:21:45 +0800

    tttt

---
+++ src/com/lyh/Test.java
@@ -0,0 +1,14 @@
+package com.lyh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args){
+        List<String> a = new ArrayList<>();
+        a.add("1");
+        a.add("2");
+        System.out.println(a.size());
+    }
+}
